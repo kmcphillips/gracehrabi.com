0c45ddb486a0efab4c8557c1acf606fc5164637e
Kevin McPhillips <github@kevinmcphillips.ca>
2010-10-12 16:52:43 -0500

    Added model validations

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,4 +1,8 @@
 class Block < ActiveRecord::Base
+  validates :body, :presence => true
+  validates :label, :presence => true
+  validates :path, :presence => true
+
 
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   acts_as_permalink
 
+  validates :body, :presence => true
+  validates :title, :presence => true, :length => {:maximum => 200}
+
 end
 
