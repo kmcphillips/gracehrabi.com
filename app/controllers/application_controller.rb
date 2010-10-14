3563e54112ad8937b1c2b422c63b598f4f03f647
Kevin McPhillips <github@kevinmcphillips.ca>
2010-10-13 20:35:22 -0500

    Moved around some helpers and laout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,6 @@ class ApplicationController < ActionController::Base
 
   include Authentication
 
-  def current_user(user=nil)
-    if user
-      raise "#{user.class} is not a User model" unless user.is_a?(User)
-      session[:current_user] = user.try(:id)
-      @current_user = user
-    else
-      @current_user || User.find_by_id(session[:current_user])
-    end
-  end
-
-  def clear_current_user
-    session[:current_user] = nil
-  end
-
-  def require_login
-    except = require_login_except rescue []
-
-    unless current_user || except.include?(params[:action])
-      flash[:error] = "You must login to view that page."
-      redirect_to "/admin/login"
-    end
-  end
-
   ## Handle custom dynamic errors
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, :with => :render_500
