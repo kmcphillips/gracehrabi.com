ec466063c5a1c76715cabfe24e1b593b4de0f4d1
Kevin McPhillips <github@kevinmcphillips.ca>
2010-10-20 22:02:37 -0500

    Cleaned up index page, error messages, date formats, and lots of CSS

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,2 +1,4 @@
 Time::DATE_FORMATS[:blog_without_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y") }
+Time::DATE_FORMATS[:event_with_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y at %I:%M%p") }
+Time::DATE_FORMATS[:index] = "%b %e/%Y %I:%M%p"
 
