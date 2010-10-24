af5371dc3852aa928783d79b5f7b33e2067256a3
Kevin McPhillips <github@kevinmcphillips.ca>
2010-10-24 09:58:26 -0500

    Got media player layout working

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,6 +4,7 @@ class TracksController < ApplicationController
   def show
     @track = Track.find(params[:id])
     @tracks = Track.order("sort_order ASC")
+    @mp3 = @track.mp3_url(request.url)
   end
 
 end
