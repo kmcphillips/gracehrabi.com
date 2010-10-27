b4b5b3f78a8a1b3a572b8dfccc65789904875a3d
Kevin McPhillips <github@kevinmcphillips.ca>
2010-10-26 21:42:33 -0500

    Changed the link style so it's in the same format as the other areas. added meta constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -8,3 +8,8 @@ PAGINATION_PER_PAGE = 5
 SITE_EMAIL = 'grace@gracehrabi.com'
 ADMIN_EMAIL = 'admin@kevinmcphillips.ca'
 
+META = {
+  :keywords => "Grace Hrabi, Grace Hrabi McPhillips, Jazz, Vocalist, Winnipeg, Manitoba, Music, Eliot Britton, Singer, Trio, Duo, Combo, Jazz Combo, Musician for hire, Wedding, Retirement, Birthday, Party, private function, Live music, live performance, Composer, Arranger, Performer, Collection Get, Ten Too Many, Universty of Manitoba, Faculty of Music, Kevin McPhillips",
+  :description => "Grace Hrabit - Vocalist"
+}.with_indifferent_access
+
