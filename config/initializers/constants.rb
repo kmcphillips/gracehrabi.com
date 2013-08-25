37d4edd5abe3d77d558c4cf7e64f6a9588fbb244
Kevin McPhillips <github@kevinmcphillips.ca>
2013-08-25 14:11:36 -0500

    Add last name spelling mistakes to meta keywords.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -11,7 +11,7 @@ ADMIN_EMAIL = 'admin@kevinmcphillips.ca'
 ROBOT_EMAIL = '"GraceHrabi.com" <robot@gracehrabi.com>'
 
 META = {
-  :keywords => "Grace Hrabi, Grace Hrabi McPhillips, Jazz, Vocalist, Winnipeg, Manitoba, Music, Eliot Britton, Singer, Trio, Duo, Combo, Jazz Combo, Musician for hire, Wedding, Retirement, Birthday, Party, private function, Live music, live performance, Composer, Arranger, Performer, Collection Get, Ten Too Many, Universty of Manitoba, Faculty of Music, Kevin McPhillips",
+  :keywords => "Grace Hrabi, Grace Hrabi McPhillips, Grace Harabi, Grace Herabi, Hrabi, Jazz, Vocalist, Winnipeg, Manitoba, Music, Eliot Britton, Singer, Trio, Duo, Combo, Jazz Combo, Musician for hire, Wedding, Retirement, Birthday, Party, private function, Live music, live performance, Composer, Arranger, Performer, Collection Get, Ten Too Many, Universty of Manitoba, Faculty of Music, Kevin McPhillips",
   :description => "Grace Hrabit - Vocalist"
 }.with_indifferent_access
 
