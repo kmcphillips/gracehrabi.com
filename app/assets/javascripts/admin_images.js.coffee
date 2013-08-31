3642fcc15eb920289b7a1b878ef880fe07609bc9
Kevin McPhillips <github@kevinmcphillips.ca>
2013-08-31 11:09:41 -0500

    Port javascript to coffeescript. Fix some deprecated _to_function stuff.

---
+++ app/assets/javascripts/admin_images.js.coffee
@@ -0,0 +1,47 @@
+$(document).ready ->
+  $("#previous_image").dialog
+    autoOpen: false
+    buttons:
+      Cancel: ->
+        $(this).dialog "close"
+
+    modal: true
+    height: 500
+    width: 700
+    title: "Select a previous image"
+
+  $(".image input.image-file-field").change ->
+    if $(".image input.image-file-field").val() and $(".image input.image-file-field").val() isnt ""
+      resetClear()
+      resetPrevious()
+
+  $(".select-previous-image-button").click ->
+    $('#previous_image').dialog('open')
+    false
+
+  $(".clear-image").click ->
+    clearImage()
+    false
+
+window.clearImage = ->
+  $("#clear_image_field").val "true"
+  resetUpload()
+  resetPrevious()
+
+window.selectImage = (id, src) ->
+  $("#previous_image").dialog "close"
+  $("#current_image").attr "src", src
+  $("#current_image").show()
+  $("#previous_image_id").val id
+  resetUpload()
+  resetClear()
+
+window.resetClear = ->
+  $("#clear_image_field").val ""
+
+window.resetUpload = ->
+  $(".image input.image-file-field").val ""
+
+window.resetPrevious = ->
+  $("#current_image").hide()
+  $("#previous_image_id").val ""
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,64 +17,64 @@ module ApplicationHelper
 
   def index_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/index.png", :alt => "Index"), path, :title => "Index", :class => "action-image"
+    html = link_to image_tag("icons/index.png", alt: "Index"), path, title: "Index", class: "action-image"
     html += "&nbsp;".html_safe + link_to(label, path) if label
     html
   end
 
   def new_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/new.png", :alt => "New"), path, :title => "New", :class => "action-image"
+    html = link_to image_tag("icons/new.png", alt: "New"), path, title: "New", class: "action-image"
     html += "&nbsp;".html_safe + link_to(label, path) if label
     html
   end
 
   def destroy_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/delete.png", :alt => "Delete"), path, :method => :delete, :confirm => "Are you sure you want to delete this?", :title => "Delete", :class => "action-image"
-    html += "&nbsp;".html_safe + link_to(label, path, :method => :delete, :confirm => "Are you sure you want to delete this?") if label
+    html = link_to image_tag("icons/delete.png", alt: "Delete"), path, method: :delete, data: {confirm: "Are you sure you want to delete this?"} , title: "Delete", class: "action-image"
+    html += "&nbsp;".html_safe + link_to(label, path, method: :delete, data: {confirm: "Are you sure you want to delete this?"}) if label
     html
   end
 
   def edit_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/edit.png", :alt => "Edit"), path, :title => "Edit", :class => "action-image"
+    html = link_to image_tag("icons/edit.png", alt: "Edit"), path, title: "Edit", class: "action-image"
     html += "&nbsp;".html_safe + link_to(label, path) if label
     html
   end
 
   def show_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/show.png", :alt => "Show"), path, :title => "Show", :class => "action-image"
+    html = link_to image_tag("icons/show.png", alt: "Show"), path, title: "Show", class: "action-image"
     html += "&nbsp;".html_safe + link_to(label, path) if label
     html
   end
 
   def copy_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("icons/copy.png", :alt => "Copy"), path, :title => "Copy", :class => "action-image"
+    html = link_to image_tag("icons/copy.png", alt: "Copy"), path, title: "Copy", class: "action-image"
     html += "&nbsp;".html_safe + link_to(label, path) if label
     html
   end
 
   def enlarge_button
-    "Enlarge&nbsp;" + image_tag("icons/magnify.png", :alt => "Enlarge", :class => :magnify)
+    "Enlarge&nbsp;" + image_tag("icons/magnify.png", alt: "Enlarge", class: :magnify)
   end
 
   def boolean_image(value)
-    image_tag("icons/#{!!value}.png", :alt => (!!value).to_s.humanize)
+    image_tag("icons/#{!!value}.png", alt: (!!value).to_s.humanize)
   end
 
   def obfuscated_mail_to(email, label=nil)
     obfuscated = email.scan(/.{1,10}/).join("[REMOVE_THIS]")
-    mail_to(email, label, :encode => "javascript") + "<noscript>#{mail_to(obfuscated, label || email.sub(/\@.*/, ""))}</noscript>".html_safe
+    mail_to(email, label, encode: "javascript") + "<noscript>#{mail_to(obfuscated, label || email.sub(/\@.*/, ""))}</noscript>".html_safe
   end
 
   def collection_index(collection, column_titles, options={}, &block)
-    content_tag(:table, :class => (options[:class] || "data"), :id => options[:id]) do
+    content_tag(:table, class: (options[:class] || "data"), id: options[:id]) do
       src = ""
 
-      src << content_tag(:tr, :class => options[:tr_class], :id => options[:tr_id]) do
+      src << content_tag(:tr, class: options[:tr_class], id: options[:tr_id]) do
         headers = ""
         column_titles.each do |title|
           headers << content_tag(:th, title)
@@ -90,8 +90,8 @@ module ApplicationHelper
 
       if collection.paginate?
         src << content_tag(:tr) do
-          content_tag(:th, :colspan => column_titles.size) do
-            will_paginate(collection, :previous_label => "<- Newer", :next_label => "Older ->")
+          content_tag(:th, colspan: column_titles.size) do
+            will_paginate(collection, previous_label: "<- Newer", next_label: "Older ->")
           end
         end
       end
@@ -101,16 +101,16 @@ module ApplicationHelper
   end
 
   def truncate_for_index(str, length = 120)
-    truncate(str, :length => length, :omission => " (more..)")
+    truncate(str, length: length, omission: " (more..)")
   end
 
   def error_messages(object)
-    render :partial => "/shared/error_messages", :object => object
+    render partial: "/shared/error_messages", object: object
   end
 
   def image_for(obj)
     if obj.respond_to?(:image) && obj.image.exists?
-      content_tag(:div, link_to(image_tag(obj.inline, :class => "inline_image", :alt => ""), obj.full, :rel => "inlinePrettyPhoto[gallery]", :class => "inline_image", :title => ""), :class => "image")
+      content_tag(:div, link_to(image_tag(obj.inline, class: "inline_image", alt: ""), obj.full, rel: "inlinePrettyPhoto[gallery]", class: "inline_image", title: ""), class: "image")
     end
   end
 
@@ -134,7 +134,7 @@ module ApplicationHelper
   end
 
   def pagination_params(opts={})
-    {:page => params[:page] || 1, :per_page => (admin? ? PAGINATION_PER_PAGE_ADMIN : PAGINATION_PER_PAGE)}.merge(opts)
+    {page: params[:page] || 1, per_page: (admin? ? PAGINATION_PER_PAGE_ADMIN : PAGINATION_PER_PAGE)}.merge(opts)
   end
 
   def row_class
