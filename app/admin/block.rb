9abe7ee5025ff983f9c9bef4a44701d23f14272d
Kevin McPhillips <github@kevinmcphillips.ca>
2013-11-02 09:10:10 -0500

    Add blocks

---
+++ app/admin/block.rb
@@ -0,0 +1,41 @@
+ActiveAdmin.register Block, as: "Section" do
+  menu label: "Sections", priority: 2
+
+  config.filters = false
+  config.batch_actions = false
+  config.paginate = false
+
+  actions :index, :edit, :update
+
+  controller do
+    def permitted_params
+      params.permit(block: [:body, :image, :clear_image, :previous_image_id])
+    end
+  end
+
+  index format: :blog, download_links: false do
+    column :label do |block|
+      block.label.humanize
+    end
+    # column :body do |post|
+    #   truncate post.body, length: 300, omission: '...'
+    # end
+    column :updated_at
+    column :age do |post|
+      "#{ distance_of_time_in_words_to_now(post.updated_at) } ago"
+    end
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :body
+    end
+    f.inputs do
+      f.template.render partial: 'admin2/attached_image', locals: {f: f}
+    end
+    
+    f.actions
+  end
+
+end
