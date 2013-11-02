f7b8efc9136eb063f3cd63b6f2644a2e207f4c11
Kevin McPhillips <github@kevinmcphillips.ca>
2013-11-01 21:29:45 -0500

    Images sorting and finished

--- app/admin/image.rb
+++ app/admin/image.rb
@@ -2,6 +2,8 @@ ActiveAdmin.register Image do
   menu label: "Images", priority: 4
 
   config.filters = false
+  config.batch_actions = false
+  config.paginate = false
 
   controller do
     def permitted_params
@@ -9,8 +11,8 @@ ActiveAdmin.register Image do
     end
   end
 
-  index as: :grid do |image|
-    link_to image_tag(image.thumb), admin2_image_path(image)
+  index download_links: false do |image|
+    render partial: 'admin2/sortable_images'
   end
 
   show title: :label do |image|
@@ -23,21 +25,22 @@ ActiveAdmin.register Image do
   end
 
   form do |f|
-    if !f.object.new_record?
-      f.inputs do 
-        f.template.render partial: 'admin2/image', locals: {f: f}
-      end
-    end
-
     f.inputs do
-      if f.object.new_record?
-        f.input :file
-      end
-
+      f.input :file
       f.input :label
     end
 
     f.actions
+  end
+
+  collection_action :sort, method: :post do
+    if params[:image].try(:is_a?, Array)
+      params[:image].each_with_index do |id, index|
+        ActiveRecord::Base.connection.execute("UPDATE images SET sort_order = #{index + 1} WHERE id = #{id.to_i}")
+      end
+    end
+
+    render nothing: true
   end
 
 end
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,2 +1,13 @@
 #= require active_admin/base
 #= require admin_images
+
+$ ->
+  $("#image_sortable").sortable update: ->
+    $.ajax
+      url: "/admin2/images/sort"
+      type: "post"
+      data: $("#image_sortable").sortable("serialize")
+      error: ->
+        alert "There was an error sorting images. Contact administrator."
+  $("#image_sortable").disableSelection()
+
