cf6e6b3d2bb1e53544aafea715a86bf523e1f95e
Kevin McPhillips <github@kevinmcphillips.ca>
2013-11-16 09:18:05 -0600

    Actually authenticate only staging

--- lib/staging_authentication.rb
+++ lib/staging_authentication.rb
@@ -2,7 +2,9 @@ module StagingAuthentication
   extend ActiveSupport::Concern
 
   included do
-    http_basic_authenticate_with name: "grace", password: "hrabi"
+    if Rails.env.staging?
+      http_basic_authenticate_with name: "grace", password: "hrabi"
+    end
   end
 
 end
