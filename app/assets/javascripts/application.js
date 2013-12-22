1b7090eca4896768038deaffa94d9bcb44058b7c
Kevin McPhillips <github@kevinmcphillips.ca>
2013-12-21 21:07:04 -0600

    Remove all assets for front end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require jquery.prettyPhoto
-//= require jquery.swfobject
-//= require jquery.timepicker
 //= require jquery.pjax
 //= require frontend
-//= require admin
-//= require admin_images
-//= require lightboxes
 //= require_self
