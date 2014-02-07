25bce326f8c2113b712e98a6fa92dcf29d54e82a
Kevin McPhillips <github@kevinmcphillips.ca>
2014-02-06 20:52:26 -0500

    Add test for archive

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,19 +5,26 @@ describe EventsController do
 
   describe "GET index" do
     it "assigns all events as @events" do
-      Event.stub(:upcoming => "upcoming", :current => "current", :past => double(:past, :order => "past"))
+      Event.stub(upcoming: "upcoming", current: "current")
       get :index
       assigns(:upcoming).should eq("upcoming")
       assigns(:current).should eq("current")
-      assigns(:past).should eq("past")
     end
   end
 
   describe "GET show" do
     it "assigns the requested event as @event" do
       Event.stub(:find).with("37") { event }
-      get :show, :id => "37"
+      get :show, id: "37"
       assigns(:event).should be(event)
+    end
+  end
+
+  describe "GET archive" do
+    it "assigns all past events as @past" do
+      Event.stub(past: double(:past, order: "past"))
+      get :archive
+      assigns(:past).should eq("past")
     end
   end
 end
