9393ff93bed6709f9d8998e62327bc448b5a9705
Kevin McPhillips <github@kevinmcphillips.ca>
2014-05-07 12:30:07 -0400

    Remove unneded broken test

--- spec/controllers/admin/contacts_controller_spec.rb
+++ spec/controllers/admin/contacts_controller_spec.rb
@@ -10,22 +10,7 @@ describe Admin::ContactsController do
       get :index
       response.should render_template(:index)
     end
-    
-    it "should get a list of contacts and set the title" do
-      contacts = [double]
-      emails = double
-      date = double
-      Contact.should_receive(:sorted).and_return(contacts)
-      contacts.should_receive(:page).and_return(contacts)
-      Contact.should_receive(:emails).and_return(emails)
-      Contact.should_receive(:last_updated_at).and_return(date)
-      get :index
-      assigns[:title].should_not be_blank
-      assigns[:contacts].should eq(contacts)
-      assigns[:emails].should eq(emails)
-      assigns[:date].should eq(date)
-    end
-    
+
     it "should search if the search term is present" do
       test = Contact.create!(email: "test@example.com")
       Contact.create!(email: "example@example.com")
