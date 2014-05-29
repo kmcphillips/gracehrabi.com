a57ab4766272d8beeea85bac2030af57d57092c0
Kevin McPhillips <github@kevinmcphillips.ca>
2014-05-28 22:39:08 -0400

    Add webhooks controller

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,30 @@
+class WebhooksController < ApplicationController
+  def create
+    @webhook = Webhook.new body: webhook_data
+
+    Rails.logger.error("Failed to save webhook! #{ @webhook.errors.full_messages }") unless @webhook.save
+
+    head(:ok)
+  end
+
+  private
+
+  def webhook_data
+    @webhook_data ||= begin
+      request.body.rewind
+      request_data = request.body.read
+      digest  = OpenSSL::Digest::Digest.new('sha256')
+      calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, Settings.shopify.shared_secret, request_data)).strip
+
+      if calculated_hmac == request.env["HTTP_X_SHOPIFY_HMAC_SHA256"]
+        request_data
+      else
+        Rails.logger.error("Failed to parse and validate webhook!")
+        Rails.logger.error("  params: #{ params }")
+        Rails.logger.error("  request_data: #{ request_data }")
+
+        nil
+      end
+    end
+  end
+end
