9affd073328810da2089fdcbd74b3eea492ff055
Kevin McPhillips <github@kevinmcphillips.ca>
2014-06-01 18:34:58 -0400

    Just print it by hand

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'digest/sha1'
 
-require 'pp'
-
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
