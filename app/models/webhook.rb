c17126c6a43c59d8f48a6d32b259d49e80229184
Kevin McPhillips <github@kevinmcphillips.ca>
2014-06-04 14:49:53 -0400

    Parse the webhook

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -2,4 +2,18 @@ class Webhook < ActiveRecord::Base
   has_many :purchases
 
   validates :body, presence: true
+
+  def as_hash
+    begin
+      @as_hash ||= JSON.parse(body)
+    rescue => e
+      ApplicationErrorJob.enqueue("Failed to parse JSON webhook.",
+        body: body,
+        error: e,
+        webhook_id: id
+      )
+
+      nil
+    end
+  end
 end
--- spec/models/webhook_spec.rb
+++ spec/models/webhook_spec.rb
@@ -8,4 +8,16 @@ describe Webhook do
       expect(webhook).to be_valid
     end
   end
+
+  describe "#as_hash" do
+    it "should parse the body JSON" do
+      expect(webhook.as_hash).to be_an_instance_of(Hash)
+    end
+
+    it "should enqueue and return nil" do
+      expect(ApplicationErrorJob).to receive(:enqueue)
+      webhook.body = "pie"
+      expect(webhook.as_hash).to be_nil
+    end
+  end
 end
