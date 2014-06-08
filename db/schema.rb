8445deebe18ab5b0e20cd2a4574b0a410dd5a4e7
Kevin McPhillips <github@kevinmcphillips.ca>
2014-06-08 09:52:03 -0400

    Add detail columns to the download

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604185139) do
+ActiveRecord::Schema.define(version: 20140608134922) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20140604185139) do
     t.boolean  "allow_anonymous",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "image_path"
   end
 
   add_index "downloads", ["filename"], name: "index_downloads_on_filename", using: :btree
--- spec/factories/downloads.rb
+++ spec/factories/downloads.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :download do
+    name "The Album"
     filename "the_album.zip"
     path "../shared/the_album-date.zip"
+    image_path "images/the-album.png"
     shopify_product_id 312 # from webhooks/order_creation.json
     allow_anonymous false
   end
