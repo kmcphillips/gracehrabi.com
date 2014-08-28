7cd5ce357811020661b0814c6daf8391d10eff7e
Kevin McPhillips <github@kevinmcphillips.ca>
2014-08-27 21:16:36 -0400

    Raise on deprecations. Infer type by file location.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@ Spork.prefork do
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.order = "random"
+    config.infer_spec_type_from_file_location!
+    config.raise_errors_for_deprecations!
   end
 end
 
